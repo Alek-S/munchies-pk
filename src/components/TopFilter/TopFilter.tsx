57a06fe24eedd7d28c9a79d86a2fdda3a2a8874d
Alek Shnayder <alek.shnayder@gmail.com>
2025-07-12 16:53:36 -0500

    feat: connect the top filter to redux filter store state

--- src/components/TopFilter/TopFilter.tsx
+++ src/components/TopFilter/TopFilter.tsx
@@ -7,11 +7,18 @@ import { Button } from '@/components/Button';
 import { BASE_URL } from '@/utils/api';
 
 import styles from './TopFilter.module.css';
+import { filtersSlice } from '@/store/slices/filters/filtersSlice';
+import { useAppDispatch } from '@/hooks/reduxHooks';
 
 export const TopFilter = (): ReactElement => {
+  const dispatch = useAppDispatch();
+
+  const categoryFilter = useSelector((state: RootState) => state.filter.category);
   const filterOptions = useSelector((state: RootState) => state.filter.filterOptions);
 
-  console.log({ filterOptions });
+  const handleClick = ({ id }: { id: string }) => {
+    dispatch(filtersSlice.actions.setCategory(categoryFilter === id ? null : id));
+  };
 
   return (
     <div className={styles.topFilter}>
@@ -20,7 +27,12 @@ export const TopFilter = (): ReactElement => {
       ) : (
         <div className={styles.filterBtnGroup}>
           {filterOptions.map((filterOption) => (
-            <Button key={filterOption.id} id={filterOption.id}>
+            <Button
+              key={filterOption.id}
+              id={filterOption.id}
+              onClick={handleClick}
+              active={categoryFilter === filterOption.id}
+            >
               <div className={styles.btnContent}>
                 <p>{filterOption.name}</p>
                 <img
